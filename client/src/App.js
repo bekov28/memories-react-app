ddb1a82a07e9d1a929752dfb3c8b7b8fafcfa98f
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-01-18 01:52:31 +0900

    posts and form

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Container, AppBar, Typography, Zoom, Grid2 } from "@mui/material";
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
+
+import memories from "./images/memories.jpg";
 
 const App = () => {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Memories
+        </Typography>
+        <img src={memories} alt="memories" height="60" />
+      </AppBar>
+      <Zoom in={true} timeout={500}>
+        <Container>
+          <Grid2
+            container
+            justifyContent="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid2 item xs={12} sm={7}>
+              <Posts />
+            </Grid2>
+            <Grid2 item xs={12} sm={4}>
+              <Form />
+            </Grid2>
+          </Grid2>
+        </Container>
+      </Zoom>
+    </Container>
   );
 };
-
 export default App;
